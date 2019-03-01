436b5b099bab1c31972cdc73886e8d997d265f7a
Victor <justicejesus1237@gmail.com>
2019-03-01 10:53:09 +0300

    обязательное задание done

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ let money = prompt("Ваш бюджет на месяц?"),
 		time = prompt("Введите дату в формате YYYY-MM-DD");	
 		console.log(money);
 		console.log(time);
-const days = 30;		
+
+let answerA = prompt("Введите обязательную статью расходов в этом месяце"),
+	answerB = prompt("Во сколько обойдется?"),
+	answerC = prompt("Введите обязательную статью расходов в этом месяце"),
+	answerD = prompt("Во сколько обойдется?")	
+	// obj[key] = value;	
+const days = 30;	
+
+
+
 
 let appData = {
 	budget: money,
 	timeData: time,
 	expenses: {
-		first: "ответ на первый вопрос: ", 
-		second: "ответ на второй вопрос: "
+		
 	},
 	optionalExpenses: {
 
@@ -22,16 +30,16 @@ let appData = {
 	savings: false
 };
 
+appData.expenses[answerA] = answerB;
+appData.expenses[answerC] = answerD;
 
 
-let answerA = prompt("Введите обязательную статью расходов в этом месяце"),
-		answerB = prompt("Во сколько обойдется?"),
-		answerC = prompt("Введите обязательную статью расходов в этом месяце"),
-		answerD = prompt("Во сколько обойдется?")
-console.log(appData.expenses.first + answerA);
-console.log(appData.expenses.second + answerB);
-console.log(appData.expenses.first + answerC);
-console.log(appData.expenses.second + answerD);
+
+// console.log(appData.expenses.first + answerA);
+// console.log(appData.expenses.second + answerB);
+// console.log(appData.expenses.first + answerC);
+// console.log(appData.expenses.second + answerD);
+console.log(appData.expenses)
 
 alert("Ваш бюджет на 1 день: " + money / days + " рублей");
 
